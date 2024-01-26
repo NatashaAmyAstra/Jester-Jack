e84a26ca13e0077df9574345317fec0205379da2
) <144343749+esmeeveldhuizen@users.noreply.github.com>
2024-01-26 20:09:33 +0100

    menu gekoppeld

---
+++ JesterJack/Assets/Scripts/Utilities.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Utilities : MonoBehaviour
+{
+    public void GoToGame()
+    {
+        SceneManager.LoadScene("esmee");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
