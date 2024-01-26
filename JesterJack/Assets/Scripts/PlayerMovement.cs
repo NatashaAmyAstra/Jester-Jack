020c047a0f3a6c5296a84d5a505462c08bf62238
) <144343749+esmeeveldhuizen@users.noreply.github.com>
2024-01-26 14:10:52 +0100

    update

--- JesterJack/Assets/Scripts/PlayerMovement.cs
+++ JesterJack/Assets/Scripts/PlayerMovement.cs
@@ -5,22 +5,37 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
-
     [SerializeField] private float XMoveRotation;
     [SerializeField] private float YMoveRotation;
-    
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     void Update()
     {
+        Vector3 positionOffset = new Vector3(0, 0, 0);
+        Quaternion rotationOffset = Quaternion.Euler(0, 0, Time.deltaTime);
+
+        positionOffset.x = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+           
+        positionOffset.z = Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
+
+        transform.position += positionOffset;
+
+
+
+        if (positionOffset  == Vector3.zero)
         {
-            Vector3 positionOffset = new Vector3(0, 0, 0);
-            Quaternion rotationOffset = Quaternion.Euler(0, 0, Time.deltaTime);
-
-            positionOffset.x = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
-            positionOffset.z = Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
-
-            transform.position += positionOffset;
+            animator.SetBool("Walking", false);
         }
+        else
+        {
+            animator.SetBool("Walking", true);
+        } 
     }
+
 }
 
