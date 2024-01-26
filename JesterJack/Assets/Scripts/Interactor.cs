93694dec53e57a5efc561c60c8b55004a0541282
) <144343749+esmeeveldhuizen@users.noreply.github.com>
2024-01-26 15:55:20 +0100

    YOO

--- JesterJack/Assets/Scripts/Interactor.cs
+++ JesterJack/Assets/Scripts/Interactor.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Interactor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Transform _interactionPoint;
+    [SerializeField] private float _interactionpointRadius = 0.5f;
+    [SerializeField] private LayerMask _interactableMask;
+
+    private readonly Collider[] _colliders = new Collider[3];
+    [SerializeField] private Collider _numFound;
+
+
+    private void Update()
     {
-        
+        _numFound = Physics.OverlapSphere(_interactionPoint.position, _interactionpointRadius)[0];
+        Column interactable = _numFound.GetComponent<Column>();
+        if(interactable != null)
+        {
+            if(Input.GetKeyDown(KeyCode.E)) {
+                interactable.Interact();
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDrawGizmos()
     {
-        
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(_interactionPoint.position, _interactionpointRadius);
     }
 }
